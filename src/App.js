3813dfe8e057827b0e53fb5914bcf64e8480ce69
returbo <returbo@mail.ru>
2020-04-24 16:28:58 +1000

    46str of book

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> 
-    </div>
-  );
+class App extends Component {
+  render() {
+    let helloWorld = {
+      user: "John",
+      password: "123456"
+    }
+    return (
+      <div className="App">
+        <h2>{helloWorld.user} = {helloWorld.password}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
