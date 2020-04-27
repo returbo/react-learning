eb8560f9c2cdeb19f671ecf2e90f07fc74dffe03
returbo <returbo@mail.ru>
2020-04-27 14:46:25 +1000

    58str of book

--- src/App.js
+++ src/App.js
@@ -2,15 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
   render() {
-    let helloWorld = {
-      user: "John",
-      password: "123456"
-    }
     return (
-      <div className="App">
-        <h2>{helloWorld.user} = {helloWorld.password}</h2>
+      <div className="App">{list.map(item =>
+            <div className="App" key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span> {item.author}</span>
+              <span> {item.num_comments}</span>
+              <span> {item.points}</span>
+            </div>
+        )}
       </div>
     );
   }
