f1ad7bb6c4b14b4a3ca1fffe59a0230c5c7c1265
returbo <returbo@mail.ru>
2020-05-19 16:45:49 +1000

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,106 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App, { Search, Button, Table } from './App';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('App', () => {
+
+  it('отрисовывает без ошибки', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('есть корректный снимок', () => {
+    const component = renderer.create(
+      <App />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  }); 
+
+});
+
+
+describe('Search', () => {
+
+  it('отрисовывает без ошибки', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Поиск</Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('есть корректный снимок', () => {
+    const component = renderer.create(
+      <Search>Поиск</Search>
+    );
+    const tree = component.toJSON(); 
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Button', () => {
+
+  it('отрисовывает без ошибки', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Button>Больше историй</Button>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('есть корректный снимок', () => {
+    const component = renderer.create(
+      <Button>Больше историй</Button>
+    );
+    const tree = component.toJSON(); 
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+
+
+describe('Table', () => {
+
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' },
+    ],
+  };
+
+
+  it('отрисовывает без ошибки', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table { ...props } />, div);
+  });
+
+  test('есть корректный снимок', () => {
+    const component = renderer.create(
+      <Table { ...props } />
+    );
+    const tree = component.toJSON(); 
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('показывает два элемента списка', () => {
+    const element = shallow(
+      <Table { ...props } />
+    );
+
+    expect(element.find('.table-row').length).toBe(2);
+  });
+
 });
